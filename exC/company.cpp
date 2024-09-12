8bfc9716b86b64a000f27e7daa6c7245c6a263fe
someone735 <81585394+someone735@users.noreply.github.com>
2024-09-12 11:49:49 -0600

    finished exC

---
+++ exC/company.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Employee{
+    private: 
+        string name;
+        string address;
+        string DOB;
+        string occupation;
+    
+    public:
+        string get_name()const;
+        void set_name(const string nam);
+
+        string get_address()const;
+        void set_address(const string newAddress);
+
+        string get_DOB()const;
+        void set_DOB(const string date);
+
+        string get_occupation() const;
+        void set_occupation(const string);
+};
+
+struct Customer{
+    private:
+        string name;
+        string address;
+        string phoneNum;
+
+    public:
+        string get_name()const;
+        void set_name(const string nam);
+
+        string get_address()const;
+        void set_address(const string newAddress);
+        
+        string get_phoneNum()const;
+        void set_phoneNum(const string phone);
+};
+
+struct Company {
+    private:
+        string companyName;
+        string companyAddress;
+        string dateEstablished;
+        vector<Employee> employees;
+        vector<Customer> customers;
+
+    public:
+        vector<Employee> get_employees() const;
+        void set_employees(const vector<Employee> newEmployees);
+        
+        vector<Customer> get_customers() const;
+        void set_customers(const vector<Customer> newCustomers);
+
+};
